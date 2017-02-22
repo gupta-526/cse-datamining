8baa9420eeb5ffdec9f63835834ea24ff1f8430c
Marielle <nwana.1@osu.edu>
2017-02-22 02:09:45 -0500

    created multiple functions.

--- ass2_gupta526_nwana1/performance_eval.py
+++ ass2_gupta526_nwana1/performance_eval.py
@@ -1,4 +1,6 @@
-def performance_rate(list):
+def performance_rate(list,threshold):
+    #call the classifier function with the given threshold
+    classifier(threshold)
     #positive is over 50k, negative is under 50k
     matrix = {"True Positive": 0, "True Negative": 0, "False Positive": 0, "False Negative": 0}
     neg=[" <=50K"," <=50K ","<=50K ","<=50K"]
@@ -18,12 +20,15 @@ def performance_rate(list):
     return matrix
 
 #slide 96
-def roc_table():
-
-def roc_plot():
-
-
-    return
+#need threshold of cutoff and tpr and fpr
+def roc(list):
+    #dummy numbers for now
+    thresholds=[1,2,3,4,5,6]
+    table=[]
+    for i in thresholds:
+        matrix=performance_rate(list,thresholds[i])
+        table.append([thresholds[i],matrix["True Positive"],matrix["False Positive"]])
+    return table
 
 def precision(matrix):
     return matrix["True Positive"]/(matrix["True Positive"]+matrix["False Positive"])
@@ -35,3 +40,18 @@ def f_measure(matrix):
     num=precision(matrix)*recall(matrix)
     denom=precision(matrix)+recall(matrix)
     return 2*num/denom
+
+#first dictionary is age probabilities, second is workclass ect
+#all from training data
+probabilities=[{0:(1/79),1:(1/79),2:(1/79)},]
+
+#where e is an iterable tuple (list)
+def posterior(e):
+    num=0
+    denom=probability[e]
+    for i in range(0,len(e)):
+        #where you look in the list at dictionary i, and in dictionary i, get the probability
+        #of the value in e[i]
+        num=num*probabilities[i[e[i]]]
+    num=num*(18/79)
+    return num/denom
