e7ed31b2327a0edba06d2ed37bf4494afb0a3cac
Marielle <nwana.1@osu.edu>
2017-02-23 00:54:20 -0500

    altered performance rate functions

--- ass2_gupta526_nwana1/performance_eval.py
+++ ass2_gupta526_nwana1/performance_eval.py
@@ -1,32 +1,54 @@
-def performance_rate(list,threshold):
-    #call the classifier function with the given threshold
-    classifier(threshold)
-    #positive is over 50k, negative is under 50k
+import csv
+
+
+def performance_rate_knn():
     matrix = {"True Positive": 0, "True Negative": 0, "False Positive": 0, "False Negative": 0}
-    neg=[" <=50K"," <=50K ","<=50K ","<=50K"]
-    pos=[">50K",">50K "," >50K", " >50K "]
-    for i in range(0, len(list)):
-        trans=list[i]
-        if trans[1] in neg:
-            if trans[2] in neg:
-                matrix["True Negative"]=matrix["True Negative"]+1
-            if trans[2] in pos:
-                matrix["False Positive"]=matrix["False Positive"]+1
-        if trans[1] in pos:
-            if trans[2] in pos:
-                matrix["True Positive"]=matrix["True Positive"]+1
-            if trans[2] in neg:
-                matrix["False Negative"]=matrix["False Negative"]+1
+    neg = [" <=50K", " <=50K ", "<=50K ", "<=50K"]
+    pos = [">50K", ">50K ", " >50K", " >50K "]
+    with open("output/knn_result_income.csv") as table:
+        #positive is over 50k, negative is under 50k
+        read=csv.reader(table)
+        next(read)
+        for row in read:
+            if row[1] in neg:
+                if row[2] in neg:
+                    matrix["True Negative"]=matrix["True Negative"]+1
+                if row[2] in pos:
+                    matrix["False Positive"]=matrix["False Positive"]+1
+            if row[1] in pos:
+                if row[2] in pos:
+                    matrix["True Positive"]=matrix["True Positive"]+1
+                if row[2] in neg:
+                    matrix["False Negative"]=matrix["False Negative"]+1
+    table.close()
+    return matrix
+def performance_rate_posterior(threshold):
+    matrix = {"True Positive": 0, "True Negative": 0, "False Positive": 0, "False Negative": 0}
+    neg = [" <=50K", " <=50K ", "<=50K ", "<=50K"]
+    pos = [">50K", ">50K ", " >50K", " >50K "]
+    with open("output/knn_result_income.csv") as table:
+        read=csv.reader(table)
+        next(read)
+        for row in read:
+            if row[1] in neg:
+                if row[-1]  <threshold:
+                    matrix["True Negative"]=matrix["True Negative"]+1
+                if row[-1] >= threshold:
+                    matrix["False Positive"]=matrix["False Positive"]+1
+            if row[1] in pos:
+                if row[-1] >= threshold:
+                    matrix["True Positive"]=matrix["True Positive"]+1
+                if row[-1] < threshold:
+                    matrix["False Negative"]=matrix["False Negative"]+1
+    table.close()
     return matrix
 
-#slide 96
-#need threshold of cutoff and tpr and fpr
-def roc(list):
+def roc():
     #dummy numbers for now
-    thresholds=[1,2,3,4,5,6]
+    thresholds=[.97,.95,.93,.81,.55,.28,.17,.15,.10,.03]
     table=[]
     for i in thresholds:
-        matrix=performance_rate(list,thresholds[i])
+        matrix=performance_rate_posterior(thresholds[i])
         table.append([thresholds[i],matrix["True Positive"],matrix["False Positive"]])
     return table
 
@@ -41,17 +63,9 @@ def f_measure(matrix):
     denom=precision(matrix)+recall(matrix)
     return 2*num/denom
 
-#first dictionary is age probabilities, second is workclass ect
-#all from training data
-probabilities=[{0:(1/79),1:(1/79),2:(1/79)},]
+def main():
+    0
 
-#where e is an iterable tuple (list)
-def posterior(e):
-    num=0
-    denom=probability[e]
-    for i in range(0,len(e)):
-        #where you look in the list at dictionary i, and in dictionary i, get the probability
-        #of the value in e[i]
-        num=num*probabilities[i[e[i]]]
-    num=num*(18/79)
-    return num/denom
+
+
+main()
