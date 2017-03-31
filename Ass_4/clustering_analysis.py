4208a619d12ed9643c012b7e01a1d3442f2c1c7f
Niharika Gupta <gupta.526@buckeyemail.osu.edu>
2017-03-31 17:24:12 -0400

    added function to get sum of squared errors

--- Ass_4/clustering_analysis.py
+++ Ass_4/clustering_analysis.py
@@ -1,9 +1,45 @@
-import os, sys
+import os, sys, math
 
+def get_input(filename):
+	data = csv.reader('Data/'+ filename, 'r')
+	x = random.choice(data[1])
+	return x
 
-def calc_sse(cluster_num,):
+def get_average(filename):
+	data = csv.reader('Data/'+filename, 'r')
+	m_i = []
+	sum_1 = 0
+	sum_2 = 0
+	sum_3 = 0
+	sum_4 = 0
+	count = 0
+	for row in data:
+		if row[3]=="1":
+			sum_1 += row[1]
+		if row[3]=="2":
+			sum_2 += row[1]
+		if row[3]=="3":
+			sum_3 += row[1]
+		if row[3]=="4":
+			sum_4 += row[1]
+		count += 1
+	avg_1 = sum_1/count
+	avg_2 = sum_2/count
+	avg_3 = sum_3/count
+	avg_4 = sum_4/count
+	m_i.append(avg_1)
+	m_i.append(avg_2)
+	m_i.append(avg_3)
+	m_i.append(avg_4)
+	return m_i
 
-	return 0
+def calc_sse(cluster_num, filename, k):
+	i=1
+	sse = 0
+	for i<=k
+		dist = sqrt(get_input(filename), get_average[i])
+		total += dist
+	return sse
 
 def calc_ssb():
 
