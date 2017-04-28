b94c4099fff96de167b7a7c9b32a0fefa3612d16
Marielle <nwana.1@osu.edu>
2017-04-28 01:02:12 -0400

    working on naive bayes

--- final-proj-gupta526-nwana1-ncasey/naive_bayes_glass.py
+++ final-proj-gupta526-nwana1-ncasey/naive_bayes_glass.py
@@ -1,24 +1,30 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-import csv, pandas as pd
-name="mushrooms.csv"
-data = pd.read_csv("Dataset/{0}".format(name))
-train, test = train_test_split(data, test_size=0.3, random_state=42)
-train, verification=train_test_split(train, test_size=0.3, random_state=42)
+import csv, pandas as pd, numpy as np
 
 
-
-
-data.to_csv("OffShelf/{0}".format(name))
-
+def predict(name):
+    data = pd.read_csv("Dataset/{0}.csv".format(name))
+    train, test = train_test_split(data, test_size=0.3, random_state=42)
+    train, verification=train_test_split(train, test_size=0.3, random_state=42)
+    gnb = GaussianNB()
+    train_class = train['class']
+    test_class = test['class']
+    verification_class = verification['class']
+    del train['class']
+    print(gnb.fit(train,train_class))
+    print(gnb.predict(verification))
+    print(gnb.predict_proba(verification))
+    pred=pd.DataFrame
+    pred['Predicted Class']=gnb.predict(verification)
+    pred['Actual Class']=verification_class
+    pred['Probability']=gnb.predict_proba(verification)
+    pred.to_csv("OffShelf/{0}".format(name))
 
 
 def main():
-    k=[2,7,2]
-    j=0
-    files=["breast_cancer.csv","mushrooms.csv","glass.csv"]
-    for i in datas:
-        analysis(i,k[j])
-        j+=1
+    files=["breast_cancer","mushrooms","glass"]
+    for i in files:
+        predict(i)
 
 main()
