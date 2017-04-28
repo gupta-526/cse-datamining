b4a5ea3727f1342e164d2ff8be313aadca8401e7
Marielle <nwana.1@osu.edu>
2017-04-27 23:45:23 -0400

    Merge branch 'master' of https://github.com/gupta-526/cse-datamining into nwana1-master
    # Conflicts:
    #	final-proj-gupta526-nwana1-ncasey/Readme

--- final-proj-gupta526-nwana1-ncasey/Marielle Files/naive_bayes_mushroom.py
+++
@@ -1,3 +0,0 @@
-from sklearn.naive_bayes import GaussianNB
-from sklearn.naive_bayes import GaussianNB
-import csv, pandas as pd
--- final-proj-gupta526-nwana1-ncasey/naive_bayes_glass.py
+++ final-proj-gupta526-nwana1-ncasey/naive_bayes_glass.py
@@ -1,3 +1,3 @@
 from sklearn.naive_bayes import GaussianNB
 import csv, pandas as pd
-
+cancer=pd.read_csv("dataset/breast_cancer.csv")
