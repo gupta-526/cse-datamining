867223a44dcf1a6b2cf6f06b00b67e0e2b062ff7
Marielle <nwana.1@osu.edu>
2017-04-29 10:49:14 -0400

    created file to tranform values to integers

--- final-proj-gupta526-nwana1-ncasey/naive_bayes.py
+++ final-proj-gupta526-nwana1-ncasey/naive_bayes.py
@@ -5,10 +5,11 @@ import pandas as pd
 
 def predict(name):
     data = pd.read_csv("Dataset/{0}.csv".format(name))
-    train, test = train_test_split(data, test_size=0.3, random_state=0)
+    train, test= train_test_split(data, test_size=0.3, random_state=0)
     gnb = GaussianNB()
     train_class = train['class']
     test_class = test['class']
+    del test['class']
     del train['class']
     gnb.fit(train,train_class)
     pred=pd.DataFrame
