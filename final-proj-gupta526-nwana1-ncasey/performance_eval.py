27cf69081a2d05bd6f9c82e6918af3b239eb01e4
Marielle <nwana.1@osu.edu>
2017-05-01 18:38:03 -0400

    outputs performance of glass

--- final-proj-gupta526-nwana1-ncasey/performance_eval.py
+++ final-proj-gupta526-nwana1-ncasey/performance_eval.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def performance_rate_knn(name):
+def performance_rate(name):
     # 1 is eat, 0 is poisonous for mushrooms, 0 for benign, 1 for malignant
     matrix = {"True Positive": 0, "True Negative": 0, "False Positive": 0, "False Negative": 0}
     with open("Naive_Output/{0}.csv".format(name)) as table:
@@ -12,14 +12,33 @@ def performance_rate_knn(name):
                 if float(row[2]) == 0:
                     matrix["True Negative"]=matrix["True Negative"]+1
                 elif float(row[2]) == 1:
-                    matrix["False Positive"]=matrix["False Positive"]+1
+                    matrix["False Negative"]=matrix["False Negative"]+1
             elif float(row[1]) == 1:
                 if float(row[2]) == 1:
                     matrix["True Positive"]=matrix["True Positive"]+1
                 elif float(row[2]) == 0:
-                    matrix["False Negative"]=matrix["False Negative"]+1
+                    matrix["False Positive"]=matrix["False Positive"]+1
     table.close()
     return matrix
+
+
+def performance_rate_glass(name):
+    # 1 is eat, 0 is poisonous for mushrooms, 0 for benign, 1 for malignant
+    matrix = {"P1-A1":0, "P1-A2":0, "P1-A3":0, "P1-A4":0, "P1-A5":0, "P1-A6":0, "P1-A7":0,
+              "P2-A1":0, "P2-A2":0, "P2-A3":0, "P2-A4":0, "P2-A5":0, "P2-A6":0, "P2-A7":0,
+              "P3-A1":0, "P3-A2":0, "P3-A3":0, "P3-A4":0, "P3-A5":0, "P3-A6":0, "P3-A7":0,
+              "P4-A1":0, "P4-A2":0, "P4-A3":0, "P4-A4":0, "P4-A5":0, "P4-A6":0, "P4-A7":0,
+              "P5-A1":0, "P5-A2":0, "P5-A3":0, "P5-A4":0, "P5-A5":0, "P5-A6":0, "P5-A7":0,
+              "P6-A1":0, "P6-A2":0, "P6-A3":0, "P6-A4":0, "P6-A5":0, "P6-A6":0, "P6-A7":0,
+              "P7-A1":0, "P7-A2":0, "P7-A3":0, "P7-A4":0, "P7-A5":0, "P7-A6":0, "P7-A7":0}
+    with open("Naive_Output/{0}.csv".format(name)) as table:
+        read=csv.reader(table)
+        next(read)
+        for row in read:
+            matrix["P{0}-A{0}".format(int(row[1]),int(row[2]))] = matrix["P{0}-A{0}".format(int(row[1]),int(row[2]))]+1
+    table.close()
+    return matrix
+
 
 def performance_rate_posterior(threshold,name):
     matrix = {"True Positive": 0, "True Negative": 0, "False Positive": 0, "False Negative": 0}
@@ -63,8 +82,8 @@ def f_measure(matrix):
     return 2*num/denom
 
 def main():
-    cancer_matrix=performance_rate_knn("breast_cancer")
-    mush_matrix=performance_rate_knn("mushrooms")
+    cancer_matrix=performance_rate("breast_cancer")
+    mush_matrix=performance_rate("mushrooms")
     print("cancer matrix")
     print(cancer_matrix)
     print("mush matrix")
@@ -80,11 +99,13 @@ def main():
     print("recall mushroom")
     print(recall(mush_matrix))
     print("f measure cancer")
-    print(f_measure(mush_matrix))
+    print(f_measure(cancer_matrix))
     print("precision cancer")
-    print(precision(mush_matrix))
+    print(precision(cancer_matrix))
     print("recall cancer")
-    print(recall(mush_matrix))
-
+    print(recall(cancer_matrix))
+    glass_matrix=performance_rate_glass("glass")
+    print("glass matrix")
+    print(glass_matrix)
 
 main()
