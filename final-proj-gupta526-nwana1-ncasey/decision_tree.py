7d438935e46f3c50ca74084d178329f7f5c9ac9f
Niharika Gupta <gupta.526@buckeyemail.osu.edu>
2017-05-02 03:50:22 -0400

    I really dont care right now

--- final-proj-gupta526-nwana1-ncasey/decision_tree.py
+++ final-proj-gupta526-nwana1-ncasey/decision_tree.py
@@ -3,7 +3,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score
 
 # split into train and test sets
@@ -29,7 +29,7 @@ def get_targets_mushroom(data):
 
 def get_targets_glass(data):
 
-    # print "Target shape", data.shape
+    print "Target shape", data.shape
     df_mod = data.values[:, 0:9]
     targets = data.values[:, 9:10]
     # print "Target shape", targets
@@ -44,7 +44,9 @@ def get_targets_glass(data):
 #     targets = data.values[:, 12:13]
 #     # print "Target shape", targets
 
-    return (df_mod, targets)
+    # return (df_mod, targets)
+
+
 def main():
 	filepath_cancer = "./Dataset/breast_cancer_binary.csv"
 	data_cancer = pd.read_csv(filepath_cancer)
@@ -61,15 +63,18 @@ def main():
 	data_mod_c, target_c = get_targets_cancer(data_cancer)
 	data_mod_m, target_m = get_targets_mushroom(data_mushroom)
 	data_mod_g, target_g = get_targets_glass(data_glass)
+	print data_mod_g.shape
+	print target_g.shape
 	# data_mod_cc, target_cc = get_targets_crime(data_crime)
 	
 	#for cancer
 	train_data, test_data, train_target, test_target = get_training_set(data_mod_c, target_c)
 		# Decision Tree model with Gini Index accuracyPython
-	decision_clf = DecisionTreeClassifier(criterion="entropy", splitter = "random", 
+	decision_clf = DecisionTreeClassifier(criterion="entropy", splitter = "best", 
 		random_state=100)
 	decision_clf_cancer = decision_clf.fit(train_data, train_target)
 	cancer_pred = decision_clf_cancer.predict(test_data)
+	export_graphviz(decision_clf_cancer,out_file="cancer.dot")
 	print "Cancer precision score", precision_score(test_target, cancer_pred)*100
 	print "Cancer Accuracy is ", accuracy_score(test_target,cancer_pred)*100
 	print "Cancer Confusion Matrix: \n", confusion_matrix(test_target, cancer_pred)
@@ -84,21 +89,24 @@ def main():
 	# print "Crime Accuracy is ", accuracy_score(test_target_cc,crime_pred)*100
 
 	#for mushroom
-	decision_clf_m = DecisionTreeClassifier(criterion="entropy", splitter = "random", 
+	decision_clf_m = DecisionTreeClassifier(criterion="entropy", splitter = "best", 
 		random_state=100)
 	train_data_m, test_data_m, train_target_m, test_target_m = get_training_set(data_mod_m, target_m)
 	decision_clf_mushroom = decision_clf_m.fit(train_data_m, train_target_m)
 	mushroom_pred = decision_clf_mushroom.predict(test_data_m)
+	export_graphviz(decision_clf_mushroom,out_file="mushroom.dot")
 	print "Mushroom Precision Score: ", precision_score(test_target_m, mushroom_pred)*100
 	print "Mushroom Accuracy is ", accuracy_score(test_target_m,mushroom_pred)*100
 	print "Mushroom Confusion Matrix: \n", confusion_matrix(test_target_m, mushroom_pred)
 
 	#for glass
-	decision_clf_g = DecisionTreeClassifier(criterion="entropy", splitter = "random", 
+	decision_clf_g = DecisionTreeClassifier(criterion="entropy", splitter = "best", 
 		random_state=100)
 	train_data_g, test_data_g, train_target_g, test_target_g = get_training_set(data_mod_g, target_g)
 	decision_clf_glass = decision_clf_g.fit(train_data_g, train_target_g)
 	glass_pred = decision_clf_glass.predict(test_data_g)
+	print glass_pred
+	export_graphviz(decision_clf_glass,out_file="glass.dot")
 	print "Glass Precision ", precision_score(test_target_g,glass_pred, average="micro")*100
 	print "Glass Accuracy is ", accuracy_score(test_target_g,glass_pred)*100
 	print "Glass Confusion Matrix: \n", confusion_matrix(test_target_g, glass_pred)
